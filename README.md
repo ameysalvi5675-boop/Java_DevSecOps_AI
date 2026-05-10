a2d5e2ffd0de5fff307d59ce3eadb662f9e55aa6
ameysalvi5675-boop <ameypro157@gmail.com>
2026-05-10 21:00:31 +0530

    Initial Spring Boot DevSecOps AI setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# Java_DevSecOps_AI
-We are building and deploying java application onto EKS using CI/CD as automation and AI to enhance the troubleshooting for the Logs so that we can get exact output which we required
---
+++ app/java-devsecops-ai/src/main/java/java_devsecops_ai/JavaDevsecopsAiApplication.java
@@ -0,0 +1,13 @@
+package com.devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevsecopsAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevsecopsAiApplication.class, args);
+    }
+
+}
